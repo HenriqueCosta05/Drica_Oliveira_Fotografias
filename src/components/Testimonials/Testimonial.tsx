429c1397b8bc8fc2c942f0deff93e160e6088a23
HenriqueCosta05 <henrique.costa10@fatec.sp.gov.br>
2024-08-23 16:01:09 -0300

    feat: formulário de contato interagindo com serviço externo

--- src/components/Testimonials/Testimonial.tsx
+++ src/components/Testimonials/Testimonial.tsx
@@ -7,7 +7,7 @@ interface TestimonialProps {
 export default function Testimonial({description, name}: TestimonialProps) {
     return (
       <>
-        <div class="bg-gradient-to-r from-white/30 via-indigo-600/30 to-indigo-700/30 p-6 rounded-lg max-w-lg relative">
+        <div class="border-2 p-6 rounded-lg max-w-lg relative">
           <div class="rounded-lg p-6 relative">
             <p class="text-lg mb-6">
               {description}
