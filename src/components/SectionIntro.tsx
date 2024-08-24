2f508cfbb844af9c24fa9db887ac3aa14308fb63
HenriqueCosta05 <henrique.costa10@fatec.sp.gov.br>
2024-08-24 13:52:08 -0300

    layout de galeria com carrosel e preparação para build

--- src/components/SectionIntro.tsx
+++ src/components/SectionIntro.tsx
@@ -6,8 +6,8 @@ interface SectionIntroProps {
     }
 export default function SectionIntro({title, description}: SectionIntroProps) {
   return (
-    <div class="flex-wrap">
-      <h1 class="text-3xl lg:text-5xl font-bold bg-gradient-to-r from-yellow-200 to-yellow-300 rounded-md w-fit my-4">
+    <div className="flex-wrap mb-8">
+      <h1 className="text-3xl lg:text-5xl font-bold bg-gradient-to-r from-yellow-200 to-yellow-300 rounded-md w-fit my-4">
         {title}
       </h1>
       <p>
---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+export { default as About } from "./About/About.astro";
+export { default as Ad } from "./Ad/Ad.astro";
+export { default as Banner } from "./Banner/Banner.astro";
+export { default as Contact } from "./Contact/Contact.astro";
+export { default as Prices } from "./Prices/Prices.astro";
+export { default as Services } from "./Services/Services.astro";
+export { default as Testimonials } from "./Testimonials/Testimonials.astro";
+export { default as NavBar } from "./NavBar.tsx";
+export { default as Footer } from "./Footer.astro";
+export { default as SectionIntro } from "./SectionIntro";
+export { default as Card } from "./Card.tsx";
+export { default as Wrapper } from "./Wrapper.astro";
+export { default as Gallery} from './Gallery/Gallery.astro';
