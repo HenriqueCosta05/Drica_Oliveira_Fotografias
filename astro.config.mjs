880149187d8314732bc6b3c9f04fbf1e193664d0
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2025-01-01 17:33:37 -0300

    incrementing features in contact form and small bug fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.dricaoliveirafotografia.com.br',
   integrations: [tailwind(), react(), sitemap({
     changefreq: 'daily',
     priority: 0.7
