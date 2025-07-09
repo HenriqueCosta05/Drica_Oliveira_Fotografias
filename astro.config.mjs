e219a90a572fbe0c4f43c3a8cd536b9d77915216
HenriqueCosta05 <henriquebenedictocostadev@gmail.com>
2025-07-09 20:34:48 -0300

    feat: update @astrojs/vercel to version 8.2.1 and configure Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import sanity from '@sanity/astro'
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.dricaoliveirafotografia.com.br',
+
   integrations: [react(), sitemap({
     changefreq: 'weekly',
     priority: 0.7
@@ -16,8 +19,12 @@ export default defineConfig({
       dataset: import.meta.env.VITE_DATASET,
       studioBasePath: '/studio',
     })],
+
   output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
